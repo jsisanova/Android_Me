0ec4d51ae49679cd0f65b351582f1559b6b14634
jsisanova <jana.sisanova@gmail.com>
2019-12-04 20:17:43 +0100

    Add click response and save InstanceState

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -26,11 +26,16 @@ import android.widget.ImageView;
 
 import com.example.android.android_me.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
+// https://github.com/udacity/Android_Me/compare/TFragments.03-Exercise-ClicksAndSavingState...TFragments.03-Solution-ClicksAndSavingState
 public class BodyPartFragment extends Fragment {
 
     // TODO(3) Create final Strings to store state information about the list of images and list index
+    // Final Strings to store state information about the list of images and list index in Bundle
+    public static final String IMAGE_ID_LIST = "image_ids";
+    public static final String LIST_INDEX = "list_index";
 
     // Tag for logging
     private static final String TAG = "BodyPartFragment";
@@ -51,6 +56,12 @@ public class BodyPartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        // Load the saved state (the list of images and list index) if there is one
+        if(savedInstanceState != null) {
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX);
+        }
+
         // Inflate the Android-Me fragment layout
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
 
@@ -65,6 +76,18 @@ public class BodyPartFragment extends Fragment {
 
             // TODO (1) Set a click listener on the image view and on a click increment the list index and set the image resource
             // TODO (2) If you reach the end of a list of images, set the list index back to 0 (the first item in the list)
+            // Set a click listener on the image view
+            imageView.setOnClickListener((View v) -> {
+                // Increment position as long as the index remains <= the size of the image ids list
+                if(mListIndex < mImageIds.size()-1) {
+                    mListIndex++;
+                } else {
+                    // The end of list has been reached, so return to beginning index
+                    mListIndex = 0;
+                }
+                // Set the image resource to the new list item
+                imageView.setImageResource(mImageIds.get(mListIndex));
+            });
 
         } else {
             Log.v(TAG, "This fragment has a null list of image id's");
@@ -86,4 +109,14 @@ public class BodyPartFragment extends Fragment {
     }
 
     // TODO (4) Override onSaveInstanceState and save the current state of this fragment
+    /**
+     * Save the current state of this fragment
+     */
+    // To save the state of a fragment, you'll need to override the onSaveInstanceState method
+    // as well as check if there is a saved state, in the host activity (AndroidMeActivity), before creating new fragment instances.
+    @Override
+    public void onSaveInstanceState(Bundle currentState) {
+        currentState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
+        currentState.putInt(LIST_INDEX, mListIndex);
+    }
 }
